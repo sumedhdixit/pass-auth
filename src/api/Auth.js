c1e2ddb49c7f5324436edf4b58eedaf86670bf61
Ratnesh <ratneshjain40@gmail.com>
2022-08-22 13:28:55 +0530

    generic fetch request

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -17,3 +17,24 @@ export const handleAuth = async (endpoint, body) => {
 	const data = await res.json();
 	return data;
 };
+
+export const fetchWithCookies = async (endpoint, body) => {
+	const res = await fetch(
+		`${process.env.REACT_APP_BACKEND_URI}/${endpoint}`,
+		{
+			method: 'POST',
+			body: JSON.stringify(body),
+			includeCredentials: true,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+
+	if (!res.ok) {
+		throw new Error(`An error has occured: ${res.status}`);
+	}
+
+	const data = await res.json();
+	return data;
+}
